3d4085c904e3310475a6b17c7eb7363747c780cf
mparje <126131806+mparje@users.noreply.github.com>
2023-05-10 05:38:38 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,14 +1,4 @@
-#!/usr/bin/python3
-""" generate.py: Generate an eBook and print-ready book format
-        from a manuscript in Markdown
-
-    Eric M. Jackson
-    start of work 2020-07-02
-
-    Generate at least part of the formatting necessary for ebook and PDF formats
-"""
-
-# import statements
+import streamlit as st
 import sys
 import os
 import unicodedata
@@ -17,50 +7,52 @@ import datetime
 # Global variables for settings:
 OUTPUT_FILE = 'output.txt'
 
-
 # helper functions
 def addparagraphs(lines):
     """Put HTML paragraph tags around each paragraph"""
-    print("Writing to file...", end='')
+    st.write("Writing to file...", end='')
     with open(OUTPUT_FILE, mode='w', encoding='utf-8-sig') as output:
         for line in lines:
             line = line.rstrip('\n')
             output.write("<p>{}</p>\n".format(line))
-    print("done.")
+    st.write("done.")
     return(0)
 
-
-# Main
 def main():
     """Run editing checks on a text document"""
 
+    st.set_page_config(page_title="Generate an eBook and print-ready book format from a manuscript in Markdown",
+                       page_icon=":books:",
+                       layout="wide")
+
+    st.title("Generate an eBook and print-ready book format from a manuscript in Markdown")
+
     # Check for proper command line usage
     if len(sys.argv) is not 2:
-        print("Usage: generate.py text_file")
-        exit(1)
+        st.error("Usage: generate.py text_file")
+        st.stop()
 
     filename = sys.argv[1]
     if not os.path.isfile(filename):
-        print("File path {} does not exist. Exiting...".format(filename))
-        exit(1)
+        st.error("File path {} does not exist. Exiting...".format(filename))
+        st.stop()
 
-    print("Started at {}, checking file {}:".format(datetime.datetime.now(), filename))
-    print("Reading file... ", end='')
+    st.write("Started at {}, checking file {}:".format(datetime.datetime.now(), filename))
+
+    st.write("Reading file... ", end='')
     try:
         with open(filename, mode='r', encoding='utf-8-sig') as file:
             # CAUTION: reading the whole file into memory
             lines = file.readlines()
-            print("done.\n")
+            st.write("done.\n")
     except IOError:
-        print("Could not read {}".format(filename))
-        exit(1)
+        st.error("Could not read {}".format(filename))
+        st.stop()
 
     exitcode = addparagraphs(lines)
     if exitcode == 0:
-        print("Finished with no errors.")
-    exit(exitcode)
-
+        st.success("Finished with no errors.")
+    st.stop()
 
 if __name__ == "__main__":
     main()
-
